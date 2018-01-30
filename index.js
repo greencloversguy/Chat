34114912e36af00413d3535a66db80caa3790dd7
greencloversguy <35625742+greencloversguy@users.noreply.github.com>
2018-01-30 21:51:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
@@ -13,6 +16,6 @@ io.on('connection', function(socket){
 	});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
