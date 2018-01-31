8b59f4822186ed6743bc5895b721ecf4199ece2e
greencloversguy <35625742+greencloversguy@users.noreply.github.com>
2018-01-31 15:50:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('https').Server(app);
 var io = require('socket.io')(http);
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
